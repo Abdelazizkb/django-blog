494c6cbd926af35fc8fd5b1145a1e2edfd8b3d3f
Abdelazizkb <61007391+Abdelazizkb@users.noreply.github.com>
2021-10-30 12:32:33 +0100

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / "db.sqlite3"),
+        'NAME':    'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
